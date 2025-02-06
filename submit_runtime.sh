f2f6930de22670e009383e557c7dfc3ce5f058c2
Frederik-DTU <s164222@student.dtu.dk>
2025-02-06 03:34:25 +0100

    Update

--- submit_runtime.sh
+++ submit_runtime.sh
@@ -1,6 +1,6 @@
     #! /bin/bash
     #BSUB -q gpuv100
-    #BSUB -J Fmnist8_025_ADAadagrad
+    #BSUB -J FHyperParaboloid2_025_ADAadagrad
     #BSUB -n 4
     #BSUB -gpu "num=1:mode=exclusive_process"
     #BSUB -W 24:00
@@ -18,16 +18,16 @@
     module swap python3/3.10.12
     
     python3 runtime.py \
-        --manifold mnist \
+        --manifold HyperParaboloid \
         --geometry Finsler \
-        --dim 8 \
+        --dim 2 \
         --batch_size 0.25 \
         --N_data 1000 \
         --T 100 \
         --v0 1.5 \
         --method ADAadagrad \
         --jax_lr_rate 0.01 \
-        --tol 0.001 \
+        --tol 0.0001 \
         --max_iter 1000 \
         --number_repeats 5 \
         --timing_repeats 5 \
